fb195ffc78d04bf5bd51cf8f3efab8c60ac15bc4
fscholsen <fscholsen@gmail.com>
2021-02-10 00:15:39 +0100

    Update build configuration to allow bundle using ES6+ features with the runtime feature of Babel, edit components

--- packages/flist-input/src/flist-input.ts
+++ packages/flist-input/src/flist-input.ts
@@ -13,16 +13,60 @@ export class FlistInput extends LitElement {
   @property()
   name = "default";
 
+  @property()
+  placeholder = "";
+
   static styles: CSSResult = css`
     :host {
+      display: inline-flex;
+      flex: 1 1 12rem;
+      min-width: 2rem;
+      height: 100%;
+      max-width: 32rem;
+      max-height: 3rem;
     }
     input {
-      background-color: red;
+      background-color: #cecece;
+      border: 1px solid #cecece;
+      border-radius: 0.25rem;
+      padding: 0.5rem;
+      outline: none;
+      box-shadow: 0 0 1px 0 rgba(0, 0.2, 0, 0.2),
+        0 0 1px 1px rgba(0, 0, 0, 0.14);
+      transition: box-shadow 0.3s ease;
+      width: 100%;
+    }
+    input:focus {
+      border: 1px solid rgba(0, 0.2, 0, 0.25);
+      box-shadow: 0 0 2px 1px rgba(0, 0.2, 0, 0.25);
     }
   `;
 
+  async firstUpdated() {
+    await new Promise((r) => setTimeout(r, 0));
+    let res = await this.test();
+    console.log(res);
+
+    this.addEventListener("focus", this._handleFocus);
+  }
+
+  private _handleFocus() {
+    // console.log("focused");
+  }
+
+  async test(): Promise<string> {
+    return new Promise((resolve) => {
+      return resolve("loaded");
+    });
+  }
+
   render(): TemplateResult {
-    return html`<input type="text" name="${this.name}" />`;
+    return html`<input
+      type="text"
+      name="${this.name}"
+      autocomplete="off"
+      placeholder="${this.placeholder}"
+    />`;
   }
 }
 
--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import { babel } from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
 import path from "path";
 
 // The name of the package must match the directory name
@@ -10,6 +11,10 @@ const extensions = [".js", ".ts"];
 
 export default {
   input,
+  // This is referenced in https://github.com/rollup/plugins/tree/master/packages/babel#babelhelpers
+  // When using with 'babelHelpers: "runtime"' (to allow async/await), it should be used as 'external' in Rollup config
+  // but it results in fail to resolve module specifier
+  // external: (id) => id.includes("@babel/runtime"),
   output: {
     dir: outputDir,
     format: "esm",
@@ -17,13 +22,20 @@ export default {
   },
   plugins: [
     resolve({ extensions }),
+    // plugin-commonjs must be placed before plugin-babel
     commonjs({ include: /node_modules/ }),
     babel({
-      babelHelpers: "bundled",
+      babelHelpers: "runtime",
       extensions,
       include: ["src/**/*"],
       exclude: ["./node_modules/*"],
-      presets: ["@babel/preset-env", "@babel/preset-typescript"],
+      presets: [["@babel/preset-env"], ["@babel/preset-typescript"]],
+      plugins: [
+        ["@babel/plugin-proposal-decorators", { decoratorsBeforeExport: true }],
+        ["@babel/plugin-proposal-class-properties"],
+        ["@babel/plugin-transform-runtime", { useESModules: true }],
+      ],
     }),
+    typescript({ tsconfig: "tsconfig.types.json" }),
   ],
 };
